9caeff791cc181418a7719323864227dcc46af68
ayush bairagi <webofayush7@gmail.com>
2025-05-25 15:47:02 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Script async src="https://ayoni.vercel.app/tracker.js" data-website-id="cmb3i7day0001l504gj29p72c"></Script>
     </html>
   );
 }
